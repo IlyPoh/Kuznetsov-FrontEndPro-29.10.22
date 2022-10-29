f0f940b503953e9291bd3aa10cb06c6771749f76
IlyPoh <pohnv888888@gmail.com>
2022-10-29 19:51:46 +0300

    Added checker and replaced parseInt

--- scripts/SimpleMathAlertWithAsking.js
+++ scripts/SimpleMathAlertWithAsking.js
@@ -2,34 +2,40 @@
 const mathOperation = prompt('Hi! Choose operation u want to do (add, sub, mult, div, +, -, *, /)')
 
 // Asking user for numbers
-const firstNumber = prompt('Lets do some math, enter first number', 1);
-const secondNumber = prompt('Ok, now enter second number', 1);
+const firstNumber = parseInt(prompt('Lets do some math, enter first number', 1));
+const secondNumber = parseInt(prompt('Ok, now enter second number', 1));
 
 // Displaying operation and numbers in console
 console.log(`Operation: ${mathOperation}`);
 console.log(`First number: ${firstNumber}`);
 console.log(`Second number: ${secondNumber}`);
 
-// Simple math
-const mathPlus = (parseInt(firstNumber)+parseInt(secondNumber));
-const mathMinus = (parseInt(firstNumber)-parseInt(secondNumber));
-const mathMultiplication = (parseInt(firstNumber)*parseInt(secondNumber));
-const mathDivision = (parseInt(firstNumber)/parseInt(secondNumber));
-
-// Displaying result
-if (mathOperation == "add" || mathOperation == "+") {
-    console.log (`Ok, here's ur result:\n${firstNumber} + ${secondNumber} = ${mathPlus}`);
-    alert(`Ok, here's ur result:\n${firstNumber} + ${secondNumber} = ${mathPlus}`);
-} else if (mathOperation == "sub" || mathOperation == "-") {
-    console.log (`Ok, here's ur result:\n${firstNumber} - ${secondNumber} = ${mathMinus}`);
-    alert(`Ok, here's ur result:\n${firstNumber} - ${secondNumber} = ${mathMinus}`);
-} else if (mathOperation == "mult" || mathOperation == "*") {
-    console.log (`Ok, here's ur result:\n${firstNumber} * ${secondNumber} = ${mathMultiplication}`);
-    alert(`Ok, here's ur result:\n${firstNumber} * ${secondNumber} = ${mathMultiplication}`);
-} else if (mathOperation == "div" || mathOperation == "/") {
-    console.log (`Ok, here's ur result:\n${firstNumber} / ${secondNumber} = ${mathDivision}`);
-    alert(`Ok, here's ur result:\n${firstNumber} / ${secondNumber} = ${mathDivision}`);
+// Checking if user entered a number
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    console.log('You did something wrong, please start from the beggining')
+    alert('You did something wrong, please start from the beggining')
 } else {
-    console.log ("You did smth wrong, refresh and start from beggining");
-    alert("You did smth wrong, refresh and start from beggining");
-};
+    // Simple math
+    const mathPlus = firstNumber + secondNumber;
+    const mathMinus = firstNumber - secondNumber;
+    const mathMultiplication = firstNumber * secondNumber;
+    const mathDivision = firstNumber / secondNumber;
+
+    // Displaying result
+    if (mathOperation == "add" || mathOperation == "+") {
+        console.log (`Ok, here's ur result:\n${firstNumber} + ${secondNumber} = ${mathPlus}`);
+        alert(`Ok, here's ur result:\n${firstNumber} + ${secondNumber} = ${mathPlus}`);
+    } else if (mathOperation == "sub" || mathOperation == "-") {
+        console.log (`Ok, here's ur result:\n${firstNumber} - ${secondNumber} = ${mathMinus}`);
+        alert(`Ok, here's ur result:\n${firstNumber} - ${secondNumber} = ${mathMinus}`);
+    } else if (mathOperation == "mult" || mathOperation == "*") {
+        console.log (`Ok, here's ur result:\n${firstNumber} * ${secondNumber} = ${mathMultiplication}`);
+        alert(`Ok, here's ur result:\n${firstNumber} * ${secondNumber} = ${mathMultiplication}`);
+    } else if (mathOperation == "div" || mathOperation == "/") {
+        console.log (`Ok, here's ur result:\n${firstNumber} / ${secondNumber} = ${mathDivision}`);
+        alert(`Ok, here's ur result:\n${firstNumber} / ${secondNumber} = ${mathDivision}`);
+    } else {
+        console.log ("You did smth wrong, refresh and start from beggining");
+        alert("You did smth wrong, refresh and start from beggining");
+    };
+}
