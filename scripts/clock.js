1be5b9a5ff467df638985839cd9c0f91b2e4a7fa
IlyPoh <pohnv888888@gmail.com>
2022-10-29 19:54:27 +0300

    Changing equation

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,15 +1,15 @@
 // Asking user for hours
-let hoursToConvert = parseInt(prompt('Tell me any number of hours'));
+let convertingHours = parseInt(prompt('Tell me any number of hours'));
 
 // Displaying hours in console
-console.log(`Hours: ${hoursToConvert}`);
+console.log(`Hours: ${convertingHours}`);
 
 // Checking if number of hours more or equals 0 and giving user the result
-if (hoursToConvert >= 0) {
-    const hoursConverted = hoursToConvert * 60;
+if (convertingHours >= 0) {
+    convertingHours *= 60;
 
-    console.log(`That's how many seconds in those hours: ${hoursConverted}`);
-    alert(`That's how many seconds in those hours: ${hoursConverted}`);
+    console.log(`That's how many seconds in those hours: ${convertingHours}`);
+    alert(`That's how many seconds in those hours: ${convertingHours}`);
 } else {
     console.log('You did something wrong, start from the beggining.')
     alert('U did smth wrong, start from the beggining.')
